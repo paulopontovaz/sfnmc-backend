cbd635cb8307963cfaf05d864a1c33ff16454e52
Paulo Vaz <paulopontovaz@gmail.com>
2021-08-15 16:26:11 +0200

    Refatorar forma de adicionar o país aos destinos

--- backend/express_server.js
+++ backend/express_server.js
@@ -13,8 +13,8 @@ app.use(express.json());
 app.listen(3333, () => console.log("Server started"));
 
 const amadeus = new Amadeus({
-  clientId: "GShykh1Kr4v6H8ArbLzCjFfJuu8EZVXB",
-  clientSecret: "m84NSPakMMJVSVG3",
+	clientId: "GShykh1Kr4v6H8ArbLzCjFfJuu8EZVXB",
+	clientSecret: "m84NSPakMMJVSVG3",
 });
 
 // let cheapestFlightDestinations = [
@@ -36,131 +36,111 @@ const amadeus = new Amadeus({
 //   { destinationIata: "LAX", price: "451.41", currency: "EUR" },
 // ];
 let cheapestFlightDestinations = [];
-let locations = [];
+let locations = {};
 // let originIATA;
 
 const getCheapestFlightDestinations = async () => {
-  try {
-    console.log("making an api request...");
-    const response = await amadeus.shopping.flightDestinations.get({
-      origin: "MAD",
-    });
+	try {
+		console.log("making an api request...");
+		const response = await amadeus.shopping.flightDestinations.get({
+			origin: "MAD",
+		});
 
-    console.log("passou");
+		console.log("passou");
 
-    const handledDestinations = await handleCheapestFlightDestinations(
-      response
-    );
-    cheapestFlightDestinations = handledDestinations;
-    console.log("the cheapest destinations to go to have been set!");
-    console.log(cheapestFlightDestinations);
-  } catch (err) {
-    console.error(err);
-  }
+		const handledDestinations = await handleCheapestFlightDestinations(
+			response
+		);
+		cheapestFlightDestinations = handledDestinations;
+		console.log("the cheapest destinations to go to have been set!");
+		console.log(cheapestFlightDestinations);
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 const handleCheapestFlightDestinations = (response) => {
-  console.log("filtering the api response...");
-  const { data, meta } = response.result;
+	console.log("filtering the api response...");
+	const { data, meta } = response.result;
 
-  return data.map((item) => ({
-    destinationIata: item.destination,
-    price: item.price.total,
-    currency: meta.currency,
-  }));
+	return data.map((item) => ({
+		destinationIata: item.destination,
+		price: item.price.total,
+		currency: meta.currency,
+	}));
 };
 
-async function processLineByLine(path, action) {
-  const fileStream = fs.createReadStream(path);
+const processLineByLine = async (path, lineCallback) => {
+	const fileStream = fs.createReadStream(path);
+	const rl = readline.createInterface({
+		input: fileStream,
+		crlfDelay: Infinity,
+	});
 
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity,
-  });
-  // Note: we use the crlfDelay option to recognize all instances of CR LF
-  // ('\r\n') in input.txt as a single line break.
-
-  for await (const line of rl) {
-    // Each line in input.txt will be successively available here as `line`.
-    action(line);
-    // console.log(`Line from file: ${line}`);
-  }
-}
+	rl.on("line", await lineCallback);
+};
 
 const setDestinationCountryCode = () => {
-  cheapestFlightDestinations.forEach((destination) => {
-    locations.forEach((location) => {
-      if (location.iataCode === destination.destinationIata) {
-        const index = cheapestFlightDestinations.indexOf(destination);
-        cheapestFlightDestinations[index] = {
-          destinationIata: destination.destinationIata,
-          countryCode: location.country,
-          flightPrice: destination.price,
-          currency: destination.currency,
-        };
-      }
-    });
-  });
+	cheapestFlightDestinations = cheapestFlightDestinations.map(
+		(destination) => ({
+			...destination,
+			countryCode: locations?.[destination.destinationIata]?.countryCode,
+		})
+	);
 };
 
 const sendRecommendationsToQueue = () => {
-  cheapestFlightDestinations.forEach((destination) => {
-    const bufferedMessage = Buffer.from(JSON.stringify(destination));
-    queueRecommendation(bufferedMessage);
-  });
+	cheapestFlightDestinations.forEach((destination) => {
+		const bufferedMessage = Buffer.from(JSON.stringify(destination));
+		queueRecommendation(bufferedMessage);
+	});
 };
 
 const queueRecommendation = (msg) => {
-  amqp.connect("amqp://localhost", function (error0, connection) {
-    if (error0) {
-      throw error0;
-    }
-    connection.createChannel(function (error1, channel) {
-      if (error1) {
-        throw error1;
-      }
+	amqp.connect("amqp://localhost", function (error0, connection) {
+		if (error0) {
+			throw error0;
+		}
+		connection.createChannel(function (error1, channel) {
+			if (error1) {
+				throw error1;
+			}
 
-      let queue = "travel_recommendation_queue";
+			let queue = "travel_recommendation_queue";
 
-      channel.assertQueue(queue, {
-        durable: true,
-      });
-      channel.sendToQueue(queue, Buffer.from(msg), {
-        persistent: true,
-      });
-      console.log(" [x] Sent '%s' to '%s'", msg, queue);
-    });
+			channel.assertQueue(queue, {
+				durable: true,
+			});
+			channel.sendToQueue(queue, Buffer.from(msg), {
+				persistent: true,
+			});
+			console.log(" [x] Sent '%s' to '%s'", msg, queue);
+		});
 
-    setTimeout(function () {
-      connection.close();
-    }, 500);
-  });
+		setTimeout(function () {
+			connection.close();
+		}, 500);
+	});
 };
 
 const Main = async () => {
-  try {
-    await processLineByLine(
-      resolve(__dirname, "..", "airports.csv"),
-      (line) => {
-        const values = line.split(",");
-        if (values[13].length) {
-          const locationInfo = {
-            country: values[8],
-            iataCode: values[13],
-            municipality: values[10],
-          };
-          locations.push(locationInfo);
-        }
-      }
-    );
-    await getCheapestFlightDestinations();
-    if (cheapestFlightDestinations.length && locations.length) {
-      setDestinationCountryCode();
-      sendRecommendationsToQueue();
-    }
-  } catch (err) {
-    console.error(err);
-  }
+	try {
+		await processLineByLine("./airports.csv", (line) => {
+			if (line?.iata_code) {
+				location[line?.iata_code] = {
+					countryCode: line.iso_country,
+					municipality: line.municipality,
+				};
+			}
+		});
+		await getCheapestFlightDestinations();
+		if (cheapestFlightDestinations.length && locations.length) {
+			setDestinationCountryCode();
+			sendRecommendationsToQueue();
+		}
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 Main();
